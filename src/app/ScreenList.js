2ba0ae955d40fc5dee755c8d346739a6878ea793
Abrar-Devs <abrar.ahmad@devsinc.com>
2023-09-15 17:08:52 +0500

    firts commit on dashboard

---
+++ src/app/ScreenList.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ScreenList = () => {
+  return (
+    <View>
+      <Text>ScreenList</Text>
+    </View>
+  );
+};
+
+export default ScreenList;
