7d4bd26a2e42c9dc656841240f0a8f5b507dad49
Abrar-Devs <abrar.ahmad@devsinc.com>
2023-09-15 16:37:29 +0500

    loadingIndicator component added

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import LoadingIndicator from '../components/common/LoadingIndicator';
 
-const LoadingScreen = () => (
-  <View style={[styles.container, styles.horizontal]}>
-    <ActivityIndicator size="large" />
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 10,
-  },
-});
+const LoadingScreen = () => <LoadingIndicator />;
 
 export default LoadingScreen;
